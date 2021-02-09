77704cf6c80694cbf307c794bf10b769236693aa
ohjiwooo <ohjiwooo@gmail.com>
2021-02-09 23:04:55 +0900

    commit

---
+++ beakjeon/n2239.py
@@ -0,0 +1,19 @@
+
+
+def check(ni,nj,board):
+
+    arr=[False]*10
+    k=ni/3*3;l=nj/3*3
+
+    for i in range(k,k+3):
+        for j in range(l,l+3):
+
+
+if __name__=='__main__':
+    board=[ [0 for i in range(9)] for j in range(9) ]
+
+    for i in range(9):
+        board[i]=list(int,input.split())
+
+    
+    
